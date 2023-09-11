21ca219ddbd2e12b1d7527043487a8da18cac61d
mistershashanksingh <mistershashanksingh@gmail.com>
2023-09-11 14:07:18 +0530

    Add chatLoop java file into main branch

---
+++ java/Adv_hacks/src/Adv_hacks/charLoop.java
@@ -0,0 +1,11 @@
+package Adv_hacks;
+
+import static java.lang.System.*;
+
+interface charLoop {
+    static void main(String[] args) {
+    for (char i = 'A'; i <= 'z'; i++){
+        out.print(String.valueOf(i));
+    }    
+    }
+}
